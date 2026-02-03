f18fd9c72cf233a6491781e15f69a0ff3c7f7bba
Jav8-Dev <javiergarrochomunoz@gmail.com>
2026-02-03 12:37:01 +0100

    Refactorizacion de clase Juego

---
+++ src/main/java/domain/Mueble.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Mueble extends Objeto {
+
+    public Mueble(String nombre, String descripcion) {
+        super(nombre, descripcion);
+    }
+    //No tienen inventariable por lo que no se pieden coger
+}
+//@author Jav8-dev|@version 1.0.
