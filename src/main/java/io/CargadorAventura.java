9985dbd18e04c5f76a3a05e15e516b6f39489ffa
k0alita <alejandrohernandossan@gmail.com>
2026-04-16 12:28:03 +0200

    archivo configuracion inicio

---
+++ src/main/java/io/CargadorAventura.java
@@ -0,0 +1,21 @@
+package io;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class CargadorAventura {
+    private Gson gson;
+    private Path directiorioBase;
+    private Path archivoAventura;
+
+    /**
+     * Lee el config.propierties
+     */
+    public void cargarConfiguracion() throws IOException {
+
+
+
+    }
+
+}
